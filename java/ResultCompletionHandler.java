f5e31f5eee74c74ef0af0f268636bf0a819366fa
Adam Bailey <adambailey.home@gmail.com>
2016-08-16 17:03:08 -0500

    Typo

--- java/ResultCompletionHandler.java
+++ java/ResultCompletionHandler.java
@@ -8,7 +8,7 @@ import org.json.JSONObject;
 
 /// Completion handler for making API Requests that returns a success indicator, a JSON object and an exception.
 /// A value for the error variable represents a client error. This is not the error returned by the API.
-/// An API returned by the API can be retrieved through the JSON object.
+/// An error returned by the API can be retrieved through the JSON object.
 /// The onResult() method is executed on the main thread.
 public interface ResultCompletionHandler {
 	void onResult(boolean success, JSONObject response, Exception exception);
