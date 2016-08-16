9a70de75e0a21367074d902c404d7c4c8e0d1bef
Adam Bailey <adambailey.home@gmail.com>
2016-08-16 08:23:22 -0500

    Java initial commit

---
+++ java/ResultCompletionHandler.java
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2016 Zello. All rights reserved.
+//
+
+package com.zellowork.apiwrapper;
+
+import org.json.JSONObject;
+
+/// Completion handler for making API Requests that returns a success indicator, a JSON object and an exception.
+/// A value for the error variable represents a client error. This is not the error returned by the API.
+/// An API returned by the API can be retrieved through the JSON object.
+/// The onResult() method is executed on the main thread.
+public interface ResultCompletionHandler {
+	void onResult(boolean success, JSONObject response, Exception exception);
+}
