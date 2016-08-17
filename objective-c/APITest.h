76a839bc670c257728eaacc1ab3a55425406eac5
Adam Bailey <adambailey.home@gmail.com>
2016-08-17 17:16:37 -0500

    Add Objective C API test class

---
+++ objective-c/APITest.h
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2016 Zello. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APITest : NSObject
+
+- (nonnull id)initWithHost:(nonnull NSString *)host apiKey:(nonnull NSString *)apiKey username:(nonnull NSString *)username password:(nonnull NSString *)password;
+
+@end
