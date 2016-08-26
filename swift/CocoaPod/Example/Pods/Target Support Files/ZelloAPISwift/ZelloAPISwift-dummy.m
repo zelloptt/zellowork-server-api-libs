275dbcfbbba183d222374665acd4f58c399973f8
Adam Bailey <adambailey.home@gmail.com>
2016-08-26 14:56:58 -0500

    Swift CocoaPod initial commit

---
+++ swift/CocoaPod/Example/Pods/Target Support Files/ZelloAPISwift/ZelloAPISwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZelloAPISwift : NSObject
+@end
+@implementation PodsDummy_ZelloAPISwift
+@end
---
+++ swift/CocoaPod/Example/Pods/Target Support Files/ZelloAPISwift/ZelloAPISwift-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "ZelloAPISwift.h"
+
+FOUNDATION_EXPORT double ZelloAPISwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZelloAPISwiftVersionString[];
+
