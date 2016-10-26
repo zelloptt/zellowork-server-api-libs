2df9ae755624bab41714d8f3a4cacdaaf6e9bed5
Adam Bailey <adambailey.home@gmail.com>
2016-10-26 17:54:48 -0500

    Update Java library to ZelloWork

--- java/ResultCompletionHandler.java
+++ java/ResultCompletionHandler.java
@@ -6,9 +6,11 @@ package com.zellowork.apiwrapper;
 
 import org.json.JSONObject;
 
-/// Completion handler for making API Requests that returns a success indicator, a JSON object and an exception.
-/// A value for the error variable represents a client error. This is not the error returned by the API.
-/// An error returned by the API can be retrieved through the JSON object.
+/**
+ * Completion handler for making API Requests that returns a success indicator, a JSON object and an exception.
+ * A value for the error variable represents a client error. This is not the error returned by the API.
+ * An error returned by the API can be retrieved through the JSON object.
+ */
 public interface ResultCompletionHandler {
-	void onResult(boolean success, JSONObject response, Exception exception);
+  void onResult(boolean success, JSONObject response, Exception exception);
 }
